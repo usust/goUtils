fa274c8b689e76ae16ec42faac67c58588291b01
lyu <vvwxu@outlook.com>
2026-02-12 15:08:42 +0800

    日志模块增加级别设置

--- logger/base.go
+++ logger/base.go
@@ -1,9 +1,21 @@
 package logger
 
-// ZapLogConfig zap配置
+const (
+	LOG_LEVEL_DEBUG  = "debug"
+	LOG_LEVEL_INFO   = "info"
+	LOG_LEVEL_WARN   = "warn"
+	LOG_LEVEL_ERROR  = "error"
+	LOG_LEVEL_DPANIC = "dpanic"
+	LOG_LEVEL_PANIC  = "panic"
+	LOG_LEVEL_FATAL  = "fatal"
+)
+
+// ZapLogConfig custom zap config
 type ZapLogConfig struct {
-	// 日志文件路径
+	// the dir of log
 	LogDir string `mapstructure:"log_dir"`
+	// 日志最小输出级别
+	LogLevel string `mapstructure:"log_level"`
 	// 单个日志文件的最大大小 (MB)
 	MaxSize int `mapstructure:"max_size"`
 	// 最多保留的旧日志文件个数
@@ -18,6 +30,10 @@ type ZapOption func(*ZapLogConfig)
 
 func ZapWithLogDir(dir string) ZapOption {
 	return func(z *ZapLogConfig) { z.LogDir = dir }
+}
+
+func ZapWithLevel(level string) ZapOption {
+	return func(z *ZapLogConfig) { z.LogLevel = level }
 }
 
 func ZapWithMaxSize(max int) ZapOption {
--- logger/zap.go
+++ logger/zap.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -11,44 +13,52 @@ import (
 
 // 对外暴露的日志对象
 var (
-	Logger        *zap.Logger
-	SugaredLogger *zap.SugaredLogger
+	Logger        = zap.NewNop()
+	SugaredLogger = Logger.Sugar()
+	loggerMu      sync.Mutex
 )
 
-// InitZapCore 初始化zap日志
+// InitZapCore
 func InitZapCore(encoder *zapcore.EncoderConfig, option ...ZapOption) error {
+	loggerMu.Lock()
+	defer loggerMu.Unlock()
+
 	var encoderConfig zapcore.EncoderConfig
 
-	// 设置日志输出格式
+	// 设置日志输出格式，如果没有传入则使用默认的配置
 	if encoder == nil {
-		encoderConfig = zapcore.EncoderConfig{
-			TimeKey:        "time",
-			LevelKey:       "level",
-			NameKey:        "logger",
-			CallerKey:      "caller",
-			MessageKey:     "msg",
-			StacktraceKey:  "stacktrace",
-			LineEnding:     zapcore.DefaultLineEnding,
-			EncodeLevel:    zapcore.LowercaseLevelEncoder,
-			EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
-			EncodeDuration: zapcore.SecondsDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder,
-		}
+		encoderConfig = defaultEncoderConfig()
 	} else {
 		encoderConfig = *encoder
 	}
 
-	// 配置JSON编码器
-	jsonEncoder := zapcore.NewJSONEncoder(encoderConfig)
-
 	// the only object of the zapConfig struct
 	zapConf := ZapLogConfig{
 		LogDir:     "./log",
+		LogLevel:   LOG_LEVEL_DEBUG,
 		MaxSize:    10,
 		MaxBackups: 30,
 		MaxAge:     180,
 		IsCompress: true,
 	}
+	for _, opt := range option {
+		if opt != nil {
+			opt(&zapConf)
+		}
+	}
+	if zapConf.LogDir == "" {
+		zapConf.LogDir = "./log"
+	}
+	if err := os.MkdirAll(zapConf.LogDir, 0o755); err != nil {
+		return err
+	}
+	minLevel, err := zapcore.ParseLevel(strings.ToLower(zapConf.LogLevel))
+	if err != nil {
+		return err
+	}
+
+	// 配置JSON编码器
+	jsonEncoder := zapcore.NewJSONEncoder(encoderConfig)
 
 	// 根据不同等级创建不同的日志文件
 	infoWriter := zapcore.AddSync(&lumberjack.Logger{
@@ -75,20 +85,20 @@ func InitZapCore(encoder *zapcore.EncoderConfig, option ...ZapOption) error {
 
 	// 设置日志级别过滤器
 	infoLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
-		return l >= zapcore.InfoLevel && l < zapcore.ErrorLevel
+		return l >= minLevel && l >= zapcore.InfoLevel && l < zapcore.ErrorLevel
 	})
 	errorLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
-		return l >= zapcore.ErrorLevel
+		return l >= minLevel && l >= zapcore.ErrorLevel
 	})
 	debugLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
-		return l == zapcore.DebugLevel
+		return l >= minLevel && l == zapcore.DebugLevel
 	})
 
 	// 控制台输出全部日志
 	consoleCore := zapcore.NewCore(
 		zapcore.NewConsoleEncoder(encoderConfig),
 		zapcore.AddSync(os.Stdout),
-		zapcore.DebugLevel,
+		minLevel,
 	)
 
 	// 合并所有Core
@@ -99,20 +109,51 @@ func InitZapCore(encoder *zapcore.EncoderConfig, option ...ZapOption) error {
 		consoleCore,
 	)
 
-	Logger = zap.New(core,
+	newLogger := zap.New(core,
 		zap.AddCaller(),
 		zap.AddCallerSkip(1),
 		zap.AddStacktrace(zap.ErrorLevel),
 	)
+	oldLogger := Logger
+	oldSugaredLogger := SugaredLogger
 
+	Logger = newLogger
 	SugaredLogger = Logger.Sugar()
-	zap.ReplaceGlobals(Logger)
+	zap.ReplaceGlobals(newLogger)
+	if oldLogger != nil {
+		_ = oldLogger.Sync()
+	}
+	if oldSugaredLogger != nil {
+		_ = oldSugaredLogger.Sync()
+	}
 
 	return nil
 }
 
+func defaultEncoderConfig() zapcore.EncoderConfig {
+	return zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+}
+
 // Sync 确保所有日志都写入磁盘
 func Sync() {
+	loggerMu.Lock()
+	defer loggerMu.Unlock()
+
+	if Logger == nil || SugaredLogger == nil {
+		return
+	}
 	err := Logger.Sync()
 	if err != nil {
 		return
